9ad623f61d841f39801b6463ce61fb5c368a9720
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 00:38:05 +0400

    Downloading of NuGet packages and create database connection

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Product>? Products { get; set; }
     }
 }
