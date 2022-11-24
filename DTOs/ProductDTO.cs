9ea250af93c08e98dadea0bb4941ff3e6d5585e0
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 00:08:00 +0400

    Creation of DTO's

---
+++ DTOs/ProductDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "name")]
+        public string? Name { get; set; }
+        [Required]
+        [Display(Name = "about")]
+        public string? About { get; set; }
+        [Required]
+        [Display(Name = "image")]
+        public string? ImagePath { get; set; }
+        [Required]
+        [Display(Name = "count of pages")]
+        public int CountOfPages { get; set; }
+        [Required]
+        [Display(Name = "price")]
+        [Range(1, 100, ErrorMessage = "The price of book must be between $1 and $100!")]
+        public double Price { get; set; }
+    }
+}
---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "full name")]
+        public string? FullName { get; set; }
+        [Required]
+        [Display(Name = "username")]
+        public string? Username { get; set; }
+        [Required]
+        [Display(Name = "email")]
+        public string? Email { get; set; }
+        [Required]
+        [Display(Name = "password")]
+        [MinLength(8, ErrorMessage = "The password must contain at least 8 characters!")]
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public string? Hash { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
