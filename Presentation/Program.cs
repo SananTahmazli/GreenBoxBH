1575e29eb83c5ba99dd81419061c4ec83ea97974
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 00:38:49 +0400

    Registering of our writing services

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,7 +1,19 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Services.Abstracts;
+using Services.Concretes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opt => opt.UseNpgsql(
+        builder.Configuration.GetConnectionString("DbConnectionForGreenBoxBH")
+        )
+    );
 
 var app = builder.Build();
 
