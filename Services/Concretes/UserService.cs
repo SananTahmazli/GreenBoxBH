68c28f5631143c507238b62eb974735dab3aeef9
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 12:09:25 +0400

    Updating the User Service

--- Services/Concretes/UserService.cs
+++ Services/Concretes/UserService.cs
@@ -2,6 +2,7 @@
 using DataAccess;
 using DataAccess.Entities;
 using DTOs;
+using Helpers;
 using Services.Abstracts;
 using System;
 using System.Collections.Generic;
@@ -17,9 +18,42 @@ namespace Services.Concretes
         {
         }
 
+        public override UserDTO Create(UserDTO dto)
+        {
+            var result = _dbContext.Users.Where(u => u.Username== dto.Username);
+
+            if (result.Any())
+            {
+                throw new Exception("Username is already taken!");
+            }
+            dto.Salt = Encryption.GenerateSalt();
+            dto.Hash = Encryption.GenerateHash(dto.Password, dto.Salt);
+            return base.Create(dto);
+        }
+
         public UserDTO Login(UserDTO dto)
         {
-            return default(UserDTO);
+            var result = _dbContext.Users.Where(u => u.Username == dto.Username);
+
+            if (result.Count() == 1)
+            {
+                var user = result.FirstOrDefault();
+                var hash = Encryption.GenerateHash(dto.Password, user.Salt);
+
+                if (hash == user.Hash)
+                {
+                    var model = _mapper.Map<User, UserDTO>(result.First());
+                    return model;
+                }
+                else
+                {
+                    throw new Exception("Password is incorrect!");
+                }
+            }
+            else
+            {
+                throw new Exception("Username is not found!");
+            }
         }
     }
 }
