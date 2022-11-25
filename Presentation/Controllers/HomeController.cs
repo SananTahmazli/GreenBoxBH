75b898084352073261e8d3375a5fc84c4301ca4f
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 11:36:34 +0400

    Creation of Controllers

---
+++ Presentation/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstracts;
+
+namespace Presentation.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromRoute] int id)
+        {
+            var product = _productService.Get(id);
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _productService.GetAll();
+            return View(productList);
+        }
+    }
+}
