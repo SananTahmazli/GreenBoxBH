fcb5191e1cb62d1c07c089b991ce7566a9ce4839
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 12:08:35 +0400

    Creation of LoginController

---
+++ Presentation/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstracts;
+
+namespace Presentation.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserDTO dto)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(UserDTO dto)
+        {
+            return View();
+        }
+    }
+}
