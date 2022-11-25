6a2e299835e98f76f5a49cccc084554636d05a85
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-25 12:07:05 +0400

    Updating the User class

--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -11,7 +11,8 @@ namespace DataAccess.Entities
         public string? FullName { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
-        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public string? Hash { get; set; }
         public DateTime BirthDate { get; set; }
     }
 }
