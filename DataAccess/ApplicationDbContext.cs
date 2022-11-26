17f068c5b55db542023d1bced4c1205d1c842a50
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-11-27 00:00:48 +0400

    Updating the ApplicationDbContext

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace DataAccess
 
         public DbSet<User>? Users { get; set; }
         public DbSet<Product>? Products { get; set; }
+        public DbSet<Cart>? Carts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
