0c53125fec0d83c3ba576058ac7911b2e48324ce
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2022-12-11 23:13:38 +0400

    Update entities

--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -13,6 +13,8 @@ namespace DataAccess.Entities
         public string? Email { get; set; }
         public string? Salt { get; set; }
         public string? Hash { get; set; }
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
         public DateTime BirthDate { get; set; }
         public List<Cart> Cart { get; set; } = new List<Cart>();
     }
